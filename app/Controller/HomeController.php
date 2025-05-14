20f4c25c706bf9b0efd66fa90142a6eacb38de4d
Jorge Peraza <jorge@monkeys.cloud>
2025-05-13 18:22:46 -0600

    include home controller content

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -1,1 +1,21 @@
 <?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use MonkeysLegion\Router\Attributes\Route;
+use MonkeysLegion\Http\Message\Response;
+use MonkeysLegion\Http\Message\Stream;
+
+final class HomeController
+{
+    #[Route('GET', '/')]
+    public function index(): Response
+    {
+        return new Response(
+            Stream::createFromString('<h2>Hello from MonkeysLegion!</h2>'),
+            200,
+            ['Content-Type' => 'text/html']
+        );
+    }
+}
