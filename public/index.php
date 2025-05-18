e20baab5578ec99eaa237161cdcf036cd39d553d
Jorge Peraza <jorge@monkeys.cloud>
2025-05-18 17:34:32 -0600

    implement middleware

--- public/index.php
+++ public/index.php
@@ -2,33 +2,54 @@
 declare(strict_types=1);
 
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use MonkeysLegion\Http\CoreRequestHandler;
 
 define('ML_BASE_PATH', dirname(__DIR__));
 require ML_BASE_PATH . '/vendor/autoload.php';
 
-// 2) Build the DI container
-$container = new MonkeysLegion\DI\ContainerBuilder()
+/* -------------------------------------------------
+ | 1) Build the DI-container
+ * ------------------------------------------------*/
+$container = (new MonkeysLegion\DI\ContainerBuilder())
     ->addDefinitions(require ML_BASE_PATH . '/config/app.php')
     ->build();
 
-// **Expose it globally for controllers to pull services from**
+/* expose it globally (legacy helpers/controllers still use it) */
 define('ML_CONTAINER', $container);
 
-// 3) Auto‑discover and register any #[Route] controllers
+/* -------------------------------------------------
+ | 2) Route auto-discovery
+ * ------------------------------------------------*/
 $container
     ->get(MonkeysLegion\Core\Routing\RouteLoader::class)
     ->loadControllers();
 
-// 4) Create PSR‑7 request and resolve the router
-$request = MonkeysLegion\Http\Message\ServerRequest::fromGlobals();
-$router  = $container->get(MonkeysLegion\Router\Router::class);
+/* -------------------------------------------------
+ | 3) Create PSR-7 request + obtain final router handler
+ * ------------------------------------------------*/
+$request  = MonkeysLegion\Http\Message\ServerRequest::fromGlobals();
+/** @var MonkeysLegion\Router\Router $router */
+$router   = $container->get(MonkeysLegion\Router\Router::class);
+/** @var ResponseFactoryInterface $responseFactory */
+$responseFactory = $container->get(ResponseFactoryInterface::class);
 
-// 5) Handle CORS preflight or add CORS headers, then dispatch
-$cors     = $container->get(MonkeysLegion\Core\Middleware\CorsMiddleware::class);
-$response = $cors(
-    $request,
-    fn(ServerRequestInterface $req) => $router->dispatch($req)
-);
+/* -------------------------------------------------
+ | 4) Build PSR-15 middleware pipeline
+ * ------------------------------------------------*/
+$core = new CoreRequestHandler($router, $responseFactory);
 
-// 6) Emit the HTTP response
-new MonkeysLegion\Http\Emitter\SapiEmitter()->emit($response);
+/* pipe middlewares in the **order you want them to run**  */
+$core->pipe($container->get(MonkeysLegion\Core\Middleware\CorsMiddleware::class));
+$core->pipe($container->get(MonkeysLegion\Http\Middleware\RateLimitMiddleware::class));
+$core->pipe($container->get(MonkeysLegion\Http\Middleware\AuthMiddleware::class));
+$core->pipe($container->get(MonkeysLegion\Http\Middleware\LoggingMiddleware::class));
+
+$response = $core->handle($request);
+
+/* -------------------------------------------------
+ | 5) Emit HTTP response
+ * ------------------------------------------------*/
+$container
+    ->get(MonkeysLegion\Http\Emitter\SapiEmitter::class)
+    ->emit($response);
