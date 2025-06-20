c7f3a45a6eea890eecbef040a294edb0d2217fd0
Jorge Peraza <jorge@monkeys.cloud>
2025-06-19 19:28:14 -0600

    fix: Register providers on ContainerBuilder before building container

--- public/index.php
+++ public/index.php
@@ -7,40 +7,36 @@ define('ML_BASE_PATH', dirname(__DIR__));
 require ML_BASE_PATH . '/vendor/autoload.php';
 
 /* -------------------------------------------------
- | 1) Build the DI-container
+ | 1) Create a mutable ContainerBuilder
  * ------------------------------------------------*/
-$container = new MonkeysLegion\DI\ContainerBuilder()
-    ->addDefinitions(require ML_BASE_PATH . '/config/app.php')
-    ->build();
+$builder = (new MonkeysLegion\DI\ContainerBuilder())
+    ->addDefinitions(require ML_BASE_PATH . '/config/app.php');
+
+/* -------------------------------------------------
+ | 1.1) Auto-register MonkeysLegion “providers”
+ * ------------------------------------------------*/
+$composer  = json_decode(file_get_contents(ML_BASE_PATH . '/composer.json'), true);
+$providers = $composer['extra']['monkeyslegion']['providers'] ?? [];
+
+foreach ($providers as $providerClass) {
+    if (class_exists($providerClass) && method_exists($providerClass, 'register')) {
+        // let the provider add its services **to the builder**
+        $providerClass::register($builder);
+    }
+}
+
+/* -------------------------------------------------
+ | 1.2) Build the immutable container *after* providers
+ * ------------------------------------------------*/
+$container = $builder->build();
 
 /* expose it globally (legacy helpers/controllers still use it) */
 define('ML_CONTAINER', $container);
 
 /* -------------------------------------------------
- | 1.1) Auto-register any MonkeysLegion “providers”
- * ------------------------------------------------*/
-// read composer.json → extra.monkeyslegion.providers
-$composer = json_decode(
-    file_get_contents(ML_BASE_PATH . '/composer.json'),
-    true
-);
-
-$providers = $composer['extra']['monkeyslegion']['providers'] ?? [];
-foreach ($providers as $providerClass) {
-    if (
-        class_exists($providerClass)
-        && method_exists($providerClass, 'register')
-    ) {
-        // let the provider bind its services into $container
-        $providerClass::register($container);
-    }
-}
-
-/* -------------------------------------------------
  | 2) Route auto-discovery
  * ------------------------------------------------*/
-$container
-    ->get(MonkeysLegion\Core\Routing\RouteLoader::class)
+$container->get(MonkeysLegion\Core\Routing\RouteLoader::class)
     ->loadControllers();
 
 /* -------------------------------------------------
@@ -52,13 +48,14 @@ $router   = $container->get(MonkeysLegion\Router\Router::class);
 /** @var ResponseFactoryInterface $responseFactory */
 $responseFactory = $container->get(ResponseFactoryInterface::class);
 
+
 /* -------------------------------------------------
  | 4) Build PSR-15 middleware pipeline
  * ------------------------------------------------*/
-$routeHandler = $container->get(MonkeysLegion\Http\RouteRequestHandler::class);
-$core         = new MonkeysLegion\Http\CoreRequestHandler($routeHandler, $responseFactory);
+$routeHandler    = $container->get(MonkeysLegion\Http\RouteRequestHandler::class);
+$responseFactory = $container->get(ResponseFactoryInterface::class);
+$core            = new MonkeysLegion\Http\CoreRequestHandler($routeHandler, $responseFactory);
 
-// pipe your middleware in the desired order
 $core->pipe($container->get(MonkeysLegion\Core\Middleware\CorsMiddleware::class));
 $core->pipe($container->get(MonkeysLegion\Http\Middleware\RateLimitMiddleware::class));
 $core->pipe($container->get(MonkeysLegion\Http\Middleware\AuthMiddleware::class));
@@ -69,6 +66,5 @@ $response = $core->handle($request);
 /* -------------------------------------------------
  | 5) Emit HTTP response
  * ------------------------------------------------*/
-$container
-    ->get(MonkeysLegion\Http\Emitter\SapiEmitter::class)
+$container->get(MonkeysLegion\Http\Emitter\SapiEmitter::class)
     ->emit($response);
